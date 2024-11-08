e948a5eaceaef4332aaca30a5790e378c4386360
Zach Tullis <ztullis@byu.edu>
2024-11-08 04:51:10 +0000

    Update app_start.sh to start index.js

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -3,5 +3,5 @@ cd /home/ec2-user/concert-app-zjt-nov6-2024
 export NVM_DIR="/home/ec2-user/.nvm" 
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-# Start the app using pm2 and specify the correct app file
-pm2 start concert-app-zjt-nov6-2024.js
+# Start the app using pm2 with the correct entry file
+pm2 start index.js
--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -4,8 +4,9 @@ export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
 # Only stop the process if it's running
-if pm2 list | grep -q "concert-app-zjt-nov6-2024"; then
-    pm2 stop concert-app-zjt-nov6-2024
+if pm2 list | grep -q "index"; then
+    pm2 stop index
 else
-    echo "concert-app-zjt-nov6-2024 is not running, nothing to stop."
+    echo "index is not running, nothing to stop."
 fi
+
