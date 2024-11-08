8e3d74b0170aa2fd93bf288929c3d83ebcf671ea
Zach Tullis <ztullis@byu.edu>
2024-11-08 04:56:57 +0000

    Update app_start.sh to use pm2 restart for graceful handling

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -3,5 +3,5 @@ cd /home/ec2-user/concert-app-zjt-nov6-2024
 export NVM_DIR="/home/ec2-user/.nvm" 
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-# Start the app using pm2 with the correct entry file
-pm2 start index.js
+# Restart the app using pm2 to avoid re-execution error
+pm2 restart index.js || pm2 start index.js
